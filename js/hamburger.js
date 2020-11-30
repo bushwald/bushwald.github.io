978c85719eee4c100cf14bf98708a72e397f31a8
bushwald <brad@bushwald.com>
2020-11-29 19:11:40 -0500

    close left nav when header, main, or footer clicked

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,15 +4,23 @@
     navToggle: document.querySelector('.nav-toggle'),
     nav: document.querySelector('nav'),
     main: document.querySelector('main'),
+    header: document.querySelector('header'),
+    footer: document.querySelector('footer'),
 
     doToggle: function(e) {
       e.preventDefault();
       this.navToggle.classList.toggle('expanded');
       this.nav.classList.toggle('expanded');
+    },
+    close: function(e) {
+      e.preventDefault();
+      this.navToggle.classList.remove('expanded');
+      this.nav.classList.remove('expanded');
     }
   };
 
   hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-  hamburger.main.addEventListener('click', function(e) { hamburger.doToggle(e); });
-
+  hamburger.main.addEventListener('click', function(e) { hamburger.close(e); });
+  hamburger.header.addEventListener('click', function(e) { hamburger.close(e); });
+  hamburger.footer.addEventListener('click', function(e) { hamburger.close(e); });
 }());
